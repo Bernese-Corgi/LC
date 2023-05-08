5206c7cd8f7c747161f1a94de17a9342295842a2
Marcella <marcella@jejememe.com>
2023-05-08 23:56:44 +0900

    프롬프트 템플릿: LLM용 프롬프트 관리

---
+++ quick-start/prompt-template.py
@@ -0,0 +1,9 @@
+from langchain import PromptTemplate
+
+
+prompt = PromptTemplate(
+    input_variables=["product"],
+    template="What is a good name for a company that makes {product}?"
+)
+
+print(prompt.format(product='colorful socks'))
